8c7879e496bce4bd362008cbf5a8af36d6c9f9b3
Divyasrirowtu <divyasrirowtu@gmail.com>
2026-09-22 20:59:18 +0530

    Complete Step 7 testing and validation

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,12 @@ load_dotenv(BASE_DIR / ".env")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+
+if POSTGRES_HOST == "postgres" and os.getenv("RUNNING_IN_DOCKER") != "true":
+    POSTGRES_HOST = "localhost"
+
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:29092")
